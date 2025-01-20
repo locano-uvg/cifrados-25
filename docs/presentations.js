4408e2e84200d2ac39efcfaf494a0e66c78068c7
Ludwing Cano <ludwing238@gmail.com>
2025-01-20 09:40:08 -0600

    adding docs

---
+++ docs/presentations.js
@@ -0,0 +1,14 @@
+export const presentations = [
+    {
+        title: "Tema 1",
+        description: "Introducción al curso y conceptos básicos",
+        path: "temas/welcome/index.html",
+        image: "temas/welcome/images/welcome.png"
+    },
+    {
+        title: "Tema 2",
+        description: "Desarrollo de conceptos avanzados",
+        path: "temas/internet/index.html",
+        image: "temas/internet/images/internet.png"
+    }
+];
