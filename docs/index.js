63114308d6da9f89dcd2547b241132b55bd1de19
Ludwing Cano <ludwing238@gmail.com>
2025-02-17 14:09:40 -0600

    simétricos

--- docs/index.js
+++ docs/index.js
@@ -4,13 +4,21 @@ import { presentations } from './presentations.js';
 const grid = document.querySelector('.presentations-grid');
 
 presentations.forEach(presentation => {
+    if (!presentation.active) {
+        return
+    }
     const card = document.createElement('div');
     card.className = 'presentation-card';
     card.innerHTML = `
-            <img src="${presentation.image}" alt="${presentation.title}" width="300">
+             <div style="display: flex; justify-content: center;">
+            <img src="${presentation.image}" alt="${presentation.title}" 
+            width="325" height="200" style="object-fit: cover;">
+            </div>
+            <div>
             <h2>${presentation.title}</h2>
             <p>${presentation.description}</p>
             <a href="${presentation.path}">Ver Presentación</a>
+            </div>
         `;
     grid.appendChild(card);
 });
--- docs/presentations.js
+++ docs/presentations.js
@@ -1,8 +1,37 @@
 export const presentations = [
     {
-        title: "Tema 1",
+        title: "Cifrados",
         description: "Conceptos básicos",
         path: "temas/cifrados/index.html",
-        image: "temas/cifrados/assets/cipher.png"
+        image: "temas/cifrados/assets/cipher.png",
+        active: true
+    },
+    {
+        title: "Cifrados Simétricos",
+        description: "Introducción a los cifrados simétricos",
+        path: "temas/simetricos/index.html",
+        image: "temas/simetricos/assets/simetricos.png",
+        active: true
+    },
+    {
+        title: "Stream Ciphers",
+        description: "Cifrados de flujo",
+        path: "temas/simetricos/stream/index.html",
+        image: "temas/simetricos/assets/stream.png",
+        active: true
+    },,
+    {
+        title: "Algoritmos Stream Ciphers",
+        description: "OTP, ChaCha20, Salsa20...",
+        path: "temas/simetricos/stream/algoritmos.html",
+        image: "temas/simetricos/assets/otp.png",
+        active: true
+    },
+    {
+        title: "Block Ciphers",
+        description: "Cifrados de bloque",
+        path: "temas/simetricos/block/index.html",
+        image: "temas/simetricos/assets/block.png",
+        active: false
     }
 ];
