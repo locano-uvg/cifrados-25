4e1d4c134eb6d26c796976046722b8158ed5370d
Ludwing Cano <ludwing238@gmail.com>
2025-03-20 12:36:09 -0600

    update(docs): adding search bar

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,3 @@
-
 import { presentations } from './presentations.js';
 
 const grid = document.querySelector('.presentations-grid');
@@ -21,4 +20,32 @@ presentations.forEach(presentation => {
             </div>
         `;
     grid.appendChild(card);
+});
+
+const searchBar = document.getElementById('search-bar');
+
+searchBar.addEventListener('input', (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    grid.innerHTML = '';
+    presentations.forEach(presentation => {
+        if (!presentation.active) {
+            return;
+        }
+        if (presentation.title.toLowerCase().includes(searchTerm) || presentation.description.toLowerCase().includes(searchTerm)) {
+            const card = document.createElement('div');
+            card.className = 'presentation-card';
+            card.innerHTML = `
+                <div style="display: flex; justify-content: center;">
+                <img src="${presentation.image}" alt="${presentation.title}" 
+                width="325" height="200" style="object-fit: cover;">
+                </div>
+                <div>
+                <h2>${presentation.title}</h2>
+                <p>${presentation.description}</p>
+                <a href="${presentation.path}">Ver Presentación</a>
+                </div>
+            `;
+            grid.appendChild(card);
+        }
+    });
 });
